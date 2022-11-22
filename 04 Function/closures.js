9f5207633cded1c5fd8023c3a02be4d77654da15
joseph muganga <jmuganga54@users.noreply.github.com>
2022-11-22 07:44:18 +0300

    Closure part 3

--- 04 Function/closures.js
+++ 04 Function/closures.js
@@ -34,3 +34,17 @@ console.log(add10(20))
 
 const add100 = createAdder(100)
 console.log(add100(-90))
+
+
+
+const createTipper = (baseTip)=>{
+     return (amount)=>{
+          return baseTip * amount
+     }
+}
+
+const tip20 = createTipper(.2)
+const tip30 = createTipper(.3)
+console.log(tip20(100))
+console.log(tip30(100))
+
